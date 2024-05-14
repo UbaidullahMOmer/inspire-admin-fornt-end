014545a0ab97b01d5b88230ebe2f3847b925b8bd
Ubaidullah Omer <121765405+UbaidullahMOmer@users.noreply.github.com>
2024-05-14 21:35:48 +0500

    Update OrderInfo.jsx

--- src/pages/orderInfo/OrderInfo.jsx
+++ src/pages/orderInfo/OrderInfo.jsx
@@ -45,7 +45,7 @@ const OrderInfo = () => {
       ) : (
         <div className="flex flex-col gap-[16px] w-full">
           <div className="flex items-center w-full px-[24px] py-[16px]">
-            <span className="text-[#737791] font-[500] w-full">Date</span>
+            {/* <span className="text-[#737791] font-[500] w-full">Date</span> */}
             <span className="text-[#737791] font-[500] w-full">Name</span>
             <span className="text-[#737791] font-[500] w-full">Email</span>
             <span className="text-[#737791] font-[500] w-full">Phone</span>
@@ -54,24 +54,25 @@ const OrderInfo = () => {
             <span className="w-[94px]"></span>
           </div>
           {orders?.map((order) => {
-            const { email, phoneNumber, totalPrice, userName, address } =
+            const { email, phone, totalPrice, name, address } =
               order || {};
+              console.log(order)
             return (
               <div
                 onClick={() => handleModel(order.id)}
-                className="flex items-center px-[24px] py-[16px] bg-[#FFF] rounded-[8px] shadow-[0_4px_20px_-0px_rgba(0,0,0,0.05)]"
+                className="flex items-center px-[24px] py-[16px] bg-[#FFF] rounded-[8px] shadow-[0_4px_40px_-0px_rgba(0,0,0,0.1)]"
               >
-                <span className="text-[#303031] font-[500] w-full">
+                {/* <span className="text-[#303031] font-[500] w-full">
                   02/07/2022
-                </span>
+                </span> */}
                 <span className="text-[#303031] font-[500] w-full">
-                  {userName}
+                  {name}
                 </span>
                 <span className="text-[#303031] font-[500] w-full">
                   {email}
                 </span>
                 <span className="text-[#303031] font-[500] w-full">
-                  {phoneNumber}
+                  {phone}
                 </span>
                 <span className="text-[#303031] font-[500] w-full">
                   ${totalPrice}
